3eeef98222cb187037acb9dcb6024695d6dc1729
Dominique Luna <dluna132@gmail.com>
2014-05-26 18:20:25 -0400

    Added client

--- auth.go
+++ auth.go
@@ -1,10 +1,21 @@
 package dogo
 
+// Auth contains data required to authenticate 
+// get DigitalOcean api.
 type Auth struct {
 	ClientID string
 	APIKey   string
 }
 
+// Client is a wrapper around Auth, Clients are used
+// to query the api.
+// 
+// To make a new Client call NewClient.
 type Client struct {
 	Auth
+}
+
+// NewClient creates a new Client.
+func NewClient(auth Auth) *Client {
+	return &Client{auth}
 }
