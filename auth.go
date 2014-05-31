1a3de3148a316612bf5537de5f9e2a880816c7b7
Dominique Luna <dluna132@gmail.com>
2014-05-31 15:08:42 -0400

    added auth tests

--- auth.go
+++ auth.go
@@ -1,5 +1,11 @@
 package dogo
 
+import (
+	"errors"
+	// "net/http"
+	"os"
+)
+
 // Auth contains data required to authenticate
 // get DigitalOcean api.
 type Auth struct {
@@ -18,4 +24,21 @@ type Client struct {
 // NewClient creates a new Client.
 func NewClient(auth Auth) *Client {
 	return &Client{auth}
+}
+
+// EnvAuth creates an Auth based on environment the
+// DIGITALOCEAN_API_KEY and DIGITALOCEAN_CLIENT_ID
+// environment variables.
+func EnvAuth() (Auth, error) {
+	var auth Auth
+	auth.APIKey = os.Getenv("DIGITALOCEAN_API_KEY")
+	auth.ClientID = os.Getenv("DIGITALOCEAN_CLIENT_ID")
+	if auth.APIKey == "" {
+		return auth, errors.New("DIGITALOCEAN_API_KEY not found in environment")
+	}
+
+	if auth.ClientID == "" {
+		return auth, errors.New("DIGITALOCEAN_CLIENT_ID not found in environment")
+	}
+	return auth, nil
 }
---
+++ auth_test.go
@@ -0,0 +1,34 @@
+package dogo
+
+import (
+	"testing"
+	"os"
+)
+
+func TestEnvAuth(t *testing.T) {
+	k := "DIGITALOCEAN_API_KEY not found in environment"
+	c := "DIGITALOCEAN_CLIENT_ID not found in environment"
+
+	auth, err := EnvAuth()
+	if err.Error() != k {
+		t.Errorf("Expected %v, got %v", k, err)
+	}
+
+	os.Setenv("DIGITALOCEAN_API_KEY", "awesomekey")
+	auth, err = EnvAuth()
+	if err.Error()  != c {
+		t.Errorf("Expected %v, got %v", c, err)
+	}
+
+	os.Setenv("DIGITALOCEAN_CLIENT_ID", "awesomeclient")
+	auth, err = EnvAuth()
+	if err != nil {
+		t.Errorf("Expected %v, got %v", nil, err)
+	}
+
+	auth2 := Auth{"awesomeclient", "awesomekey"}
+	if auth != auth2 {
+		t.Errorf("Expected %v, got %v", auth2, auth)
+	}
+
+}
