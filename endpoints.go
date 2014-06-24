a2fcf5fc602d119f24b740701d96683a8f111e26
Dominique Luna <dluna132@gmail.com>
2014-06-24 16:08:14 -0400

    redo endpoints, update api version

--- endpoints.go
+++ endpoints.go
@@ -1,21 +1,26 @@
 package dogo
 
-const (
-	// Version of the DigitalOcean API
-	Version = "v1"
-
-	// Image endpoint
-	ImagesEndpoint = "https://api.digitalocean.com/v1/images"
-
-	// Droplet endpoint
-	DropletsEndpoint = "https://api.digitalocean.com/v1/droplets"
-
-	// Region endpoint
-	RegionsEndpoint = "https://api.digitalocean.com/v1/regions"
-
-	// Sizes endpoint
-	SizesEndpoint = "https://api.digitalocean.com/v1/sizes"
-
-	// SSH Key endpoint
-	KeysEndpoint = "https://api.digitalocean.com/v1/ssh_keys"
+import (
+	"fmt"
 )
+
+const (
+	// Base URL to API
+	BaseURL = "https://api.digitalocean.com/v2"
+)
+
+var (
+	DropletsEndpoint = endpoint("droplets")
+	DomainsEndpoint  = endpoint("domains")
+	ImagesEndpoint   = endpoint("images")
+	SSHKeysEndpoint  = endpoint("sshkeys")
+	RegionsEndpoint  = endpoint("regions")
+	ActionsEndpoint  = endpoint("actions")
+	SizesEndpoint    = endpoint("sizes")
+	EventsEndpoint   = endpoint("events")
+)
+
+// Create an endpoint by appending resource to base url
+func endpoint(resource string) string {
+	return fmt.Sprintf("%s/%s", BaseURL, resource)
+}
