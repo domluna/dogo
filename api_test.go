ee4029f0439101a935c37af24717e2391b0062d2
Dominique Luna <dluna132@gmail.com>
2014-06-28 02:47:31 -0400

    fmted, first test case

--- api_test.go
+++ api_test.go
@@ -1,13 +1,30 @@
 package dogo
 
 import (
-	"testing"
-	"fmt"
-	"net/http/httptest"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
 )
+
+func TestEnvAuth(t *testing.T) {
+	msg := "DIGITALOCEAN_TOKEN not found in environment"
+
+	cli, err := EnvAuth()
+	if err.Error() != msg {
+		t.Errorf("Expected %v, got %v", msg, err)
+	}
+
+	os.Setenv("DIGITALOCEAN_TOKEN", "mytokenhere")
+	temp := Client{"mytokenhere"}
+	if cli != temp {
+		t.Errorf("Expected %v, got %v", temp, cli)
+	}
+
+}
 
 func TestGet(t *testing.T) {
 
