91d4f764e5e7740674e1b803b6d517dcd5e7f869
Dominique Luna <dluna132@gmail.com>
2014-07-09 19:04:59 -0400

    short summary of api usage, still need to document individual functions and stuff better

--- digitalocean/api_test.go
+++ digitalocean/api_test.go
@@ -23,37 +23,3 @@ func TestEnvAuth(t *testing.T) {
 		t.Errorf("Expected %v, got %v", temp, token)
 	}
 }
-
-func TestGet(t *testing.T) {
-	t.Logf("Starting Get tests ... \n")
-}
-
-// func TestCreateDroplet(t *testing.T) {
-// 	t.Logf("Starting Create Test ... \n")
-// 	c := Client{token}
-// 	dc := DropletClient{c}
-// 	droplet, err := dc.Create(map[string]interface{}{
-// 		"name":   "test-create",
-// 		"region": "nyc2",
-// 		"size":   "512mb",
-// 		"image":  4296335,
-// 	})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	t.Logf("%v\n", droplet)
-// }
-//
-// func TestPut(t *testing.T) {
-//
-// }
-//
-// func TestDelete(t *testing.T) {
-// 	t.Logf("Starting Delete Test ... \n")
-// 	c := Client{token}
-// 	dc := DropletClient{c}
-// 	err := dc.Destroy(1975885)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
--- doc.go
+++ doc.go
@@ -1,4 +1,57 @@
 /*
 Package dogo provides an client to the DigitalOcean API.
+
+The digitalocean package provides the REST API to interfact with DigitalOcean. This is
+used under the hood in the other packages which represent the digitalocean resource.
+
+Current clients include:
+
+	droplets
+	sizes
+	images
+	domains
+	actions
+	regions
+	keys
+
+Each client requires a DigitalOcean token. Tokens can either be read, write or both read/write; so make sure you have the correct token permissions.
+
+If you export the token as follows:
+
+$ export $DIGITALOCEAN_TOKEN="token"
+
+You could authenticate like so:
+
+import (
+	"github.com/domluna/dogo/digitalocean"
+)
+
+func main() {
+	token, err := digitalocean.EnvAuth()
+	if err != nil {
+		// make sure the env variable is set
+	}
+	// use token with client apis
+}
+
+For example using the droplet client will go as follows:
+
+import (
+	"github.com/domluna/dogo/droplet"
+)
+
+func main() {
+	cli := droplet.NewClient("your token here")
+
+	// get all droplets
+
+	droplets, err := cli.GetAll()
+	if err != nil {
+		// deal with error
+	}
+	// do stuff with droplets
+}
+
+All interactions to DigitalOcean take place within the client.
 */
 package dogo
